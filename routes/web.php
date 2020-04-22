04220109daec04998abcfe6eddcb43bf341d3aec
luigii1506 <luis.encinas1506@gmail.com>
2020-04-21 21:00:21 -0700

    Se agrego funcionalidad de mapa

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,24 @@ Route::get('admin/home', 'AdminController@index')->name('admin.home');
 Route::get('admin/home/giveRole', 'AdminController@giveRoleToUser')->name('admin.giveRole');
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/mapa', function(){
+    $config = array();
+    $config['center'] = 'auto';
+    $config['onboundschanged'] = 'if (!centreGot) {
+            var mapCentre = map.getCenter();
+            marker_0.setOptions({
+                position: new google.maps.LatLng(mapCentre.lat(), mapCentre.lng())
+            });
+        }
+        centreGot = true;';
+
+    app('map')->initialize($config);
+
+
+    $marker = array();
+    app('map')->add_marker($marker);
+
+    $map = app('map')->create_map();
+    echo "<html><head><script type='text/javascript'>var centreGot = false;</script>".$map['js']."</head><body>".$map['html']."</body></html>";
+});
