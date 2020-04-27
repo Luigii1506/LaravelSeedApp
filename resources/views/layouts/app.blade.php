1d77fc11ab36fb09f74255502e52c6c4b51551e5
luigii1506 <luis.encinas1506@gmail.com>
2020-04-27 12:30:21 -0700

    Se cambio texto de dialog

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -287,35 +287,34 @@
                     </footer>
                 </div>
                 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">×</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                        <div class="modal-footer">
-                            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                            <button id="logoutButton" href="{{ route('logout') }}" class="btn btn-primary" type="button" data-dismiss="modal">Logout</button>
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">¿Listo para salir?</h5>
+                                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">Presiona "Logout" si deseas terminar con tu sesión.</div>
+                            <div class="modal-footer">
+                                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+                                <button id="logoutButton" href="{{ route('logout') }}" class="btn btn-primary" type="button" data-dismiss="modal">Logout</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             
-            <div class="btn-group-fab" role="group" aria-label="FAB Menu">
-                <div>
-                    <button type="button" class="btn btn-main btn-primary has-tooltip" data-placement="left" title="Menu"> <i class="fa fa-bars"></i> </button>
-                    @can('administrar libros') 
-                        <a class="btn btn-sub btn-info has-tooltip" data-placement="left" title="Fullscreen" href="{{ route('libros.create') }}"> <i class="fa fa-plus"></i> </a>
-                    @endcan
-                    <button type="button" class="btn btn-sub btn-danger has-tooltip" data-placement="left" title="Save"> <i class="fa fa-floppy-o"></i> </button>
-                    <button type="button" class="btn btn-sub btn-warning has-tooltip" data-placement="left" title="Download"> <i class="fa fa-download"></i> </button>
+                <div class="btn-group-fab" role="group" aria-label="FAB Menu">
+                    <div>
+                        <button type="button" class="btn btn-main btn-primary has-tooltip" data-placement="left" title="Menu"> <i class="fa fa-bars"></i> </button>
+                        @can('administrar libros') 
+                            <a class="btn btn-sub btn-info has-tooltip" data-placement="left" title="Fullscreen" href="{{ route('libros.create') }}"> <i class="fa fa-plus"></i> </a>
+                        @endcan
+                        <button type="button" class="btn btn-sub btn-danger has-tooltip" data-placement="left" title="Save"> <i class="fa fa-floppy-o"></i> </button>
+                        <button type="button" class="btn btn-sub btn-warning has-tooltip" data-placement="left" title="Download"> <i class="fa fa-download"></i> </button>
+                    </div>
                 </div>
             </div>
-            </div>
-            
         </div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
