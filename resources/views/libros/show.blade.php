845ace492720a7cb819882d422b6d729e7da7465
luigii1506 <luis.encinas1506@gmail.com>
2020-04-27 12:28:03 -0700

    Dialog para poder borrar

--- resources/views/libros/show.blade.php
+++ resources/views/libros/show.blade.php
@@ -14,11 +14,11 @@
                     
                         <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink" x-placement="bottom-end" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(17px, 19px, 0px);">
                             <form action="{{ route('libros.destroy',$libro->id) }}" method="POST" id="deleteForm">  
-                                <div class="dropdown-header">Dropdown Header:</div>
+                                <div class="dropdown-header">Acciones:</div>
                             
-                                <a class="dropdown-item" id="deleteTag">Delete</a>
+                                <a class="dropdown-item" data-toggle="modal" data-target="#deleteModal">Eliminar</a>
                                 
-                                <a class="dropdown-item" href="{{ route('libros.edit',$libro->id) }}">Edit</a>
+                                <a class="dropdown-item" href="{{ route('libros.edit',$libro->id) }}">Editar</a>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#">Something else here</a>
                                 </div>
@@ -57,7 +57,27 @@
             </div>
         </div>
     </div>  
-
+    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteModalLabel">Acepta si deseas borrar el libro</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Si presionas aceptar el libro con el titulo "{{$libro->titulo}}" sera borrado permanentemente de la base de datos.</div>    
+                    <div class="modal-footer">
+                        <form action="{{ route('libros.destroy',$libro->id) }}" method="POST" id="deleteForm">  
+                            <button class="btn btn-secondary" type="butston" data-dismiss="modal">Cancelar</button>
+                            <button class="btn btn-primary" type="submit">Aceptar</button>
+                            @csrf
+                            @method('DELETE')
+                        </form>   
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div>
    
